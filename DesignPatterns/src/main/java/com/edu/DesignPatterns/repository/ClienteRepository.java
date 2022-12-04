432809c7950f5c35689a392e1d8ad06df8000f0e
afrain <calixtoafrain@gmail.com>
2022-12-04 14:52:39 -0300

    Projeto Design Patterns (Singleton, Strategy, Facade)

---
+++ DesignPatterns/src/main/java/com/edu/DesignPatterns/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.edu.DesignPatterns.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edu.DesignPatterns.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+}
